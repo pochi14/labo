b3da83d4747658268179b6e00b6476b8d3166a5c
pochi14 <165913955+pochi14@users.noreply.github.com>
2024-04-03 11:20:33 -0300

    Add files via upload

---
+++ repo-1/mor/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int miFuncionPrincipal()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    // Llama a la función miFuncionPrincipal en lugar de main
+    return miFuncionPrincipal();
+}
